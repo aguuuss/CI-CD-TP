540ecb3486d33f1ef0d43068191c288dae5b7ac0
Agus Irala <agustinirala240@gmail.com>
2026-06-04 11:09:18 -0300

    trigging error

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 export function App() {
   return (
     <main className="app-shell">
-      <h1>Hola Mundo</h1>
+      <h1>Hola</h1>
       <p>Demo simple para validar integración continua y entrega continua.</p>
     </main>
   );
